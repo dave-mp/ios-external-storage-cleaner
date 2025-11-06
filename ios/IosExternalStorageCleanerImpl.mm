a9d35846550207488691fee27880e91e19c830ec
David Mendoza <david.mendoza@dualbootpartners.com>
2025-11-06 08:51:00 -0500

    chore: enhance method export for architecture compatibility

--- ios/IosExternalStorageCleanerImpl.mm
+++ ios/IosExternalStorageCleanerImpl.mm
@@ -4,24 +4,24 @@
 
 RCT_EXPORT_MODULE(IosExternalStorageCleaner)
 
-// Synchronous method implementation for both architectures
+#ifdef RCT_NEW_ARCH_ENABLED
+// TurboModule implementation for new architecture
 - (NSNumber *)multiply:(double)a b:(double)b {
     NSNumber *result = @(a * b);
     return result;
 }
 
-#ifndef RCT_NEW_ARCH_ENABLED
-// For old architecture, export the method as synchronous
-RCT_EXPORT_SYNCHRONOUS_TYPED_METHOD(NSNumber *, multiply:(double)a b:(double)b)
-{
-    return [self multiply:a b:b];
-}
-#else
-// For new architecture, provide getTurboModule
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
     return std::make_shared<facebook::react::NativeIosExternalStorageCleanerSpecJSI>(params);
+}
+#else
+// Old architecture - export synchronous method
+RCT_EXPORT_SYNCHRONOUS_TYPED_METHOD(NSNumber *, multiply:(double)a b:(double)b)
+{
+    NSNumber *result = @(a * b);
+    return result;
 }
 #endif
 
