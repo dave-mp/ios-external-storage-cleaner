90c4b48ae55e56d909354fc04141928d2a731915
David Mendoza <david.mendoza@dualbootpartners.com>
2025-11-06 21:37:31 -0500

    chore: add unsupported feature handling for Android in IosExternalStorageCleanerModule

--- android/src/main/java/com/iosexternalstoragecleaner/IosExternalStorageCleanerModule.kt
+++ android/src/main/java/com/iosexternalstoragecleaner/IosExternalStorageCleanerModule.kt
@@ -1,6 +1,7 @@
 package com.iosexternalstoragecleaner
 
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.Promise
 import com.facebook.react.module.annotations.ReactModule
 
 @ReactModule(name = IosExternalStorageCleanerModule.NAME)
@@ -15,6 +16,14 @@ class IosExternalStorageCleanerModule(reactContext: ReactApplicationContext) :
   // See https://reactnative.dev/docs/native-modules-android
   override fun multiply(a: Double, b: Double): Double {
     return a * b
+  }
+
+  override fun pickFolder(promise: Promise) {
+    promise.reject("UNSUPPORTED", "This feature is only available on iOS")
+  }
+
+  override fun cleanWithBookmark(bookmarkBase64: String, promise: Promise) {
+    promise.reject("UNSUPPORTED", "This feature is only available on iOS")
   }
 
   companion object {
